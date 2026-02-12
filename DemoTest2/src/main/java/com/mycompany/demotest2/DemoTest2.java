b425308e7621ba48f087ea2ddabe49828ba9fd42
Nirali Patel <pate5855@sheridancollege.ca>
2026-02-12 17:14:37 -0500

    commit 1

---
+++ DemoTest2/src/main/java/com/mycompany/demotest2/DemoTest2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.demotest2;
+
+/**
+ *
+ * @author Nirali
+ */
+public class DemoTest2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
